8a03f48d92fc6c5e07f7f699f4a1a6858e6c0503
dilgerma <martin.dilger@pentasys.de>
2010-07-21 21:30:25 +0200

    pom cleanup, add Facade API and Implementation

---
+++ src/main/java/de/pentasys/k/domain/SubmitOrderFailedException.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package de.pentasys.k.domain;
+
+/**
+ * @author <a href="mailto:martin.dilger@pentasys.de">Martin Dilger</a>
+ * @since 21.07.2010
+ */
+public class SubmitOrderFailedException extends Exception {
+
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 1L;
+
+    public SubmitOrderFailedException(String message, Throwable t){
+	super(message, t);
+    }
+}
---
+++ src/main/java/de/pentasys/k/domain/VideoStoreFacade.java
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package de.pentasys.k.domain;
+
+import java.util.List;
+
+
+/**
+ * A Facade to encapsulate the Backend Access and provide common functionality
+ * for the whole application.
+ *
+ * @author <a href="mailto:martin.dilger@pentasys.de">Martin Dilger</a>
+ * @since 21.07.2010
+ */
+public interface VideoStoreFacade {
+
+    public List<Movie> getMovies();
+
+    public void submitOrder(Customer customer, List<Movie> movies) throws SubmitOrderFailedException;
+
+    public Movie getDailyOffer();
+
+}
