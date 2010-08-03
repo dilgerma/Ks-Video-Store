8de7fa12f790c7279197d9603c021a51cb95c1f6
dilgerma <martin.dilger@pentasys.de>
2010-08-03 20:15:07 +0200

    switched from listviews to datatable

---
+++ src/main/java/de/pentasys/k/pages/MovieDataProvider.java
@@ -0,0 +1,103 @@
+/**
+ *
+ */
+package de.pentasys.k.pages;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.extensions.markup.html.repeater.util.SortParam;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.injection.web.InjectorHolder;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import de.pentasys.k.domain.Movie;
+import de.pentasys.k.domain.VideoStoreFacade;
+
+/**
+ * @author <a href="mailto:martin.dilger@pentasys.de">Martin Dilger</a>
+ * @since 03.08.2010
+ */
+public class MovieDataProvider extends SortableDataProvider<Movie>{
+
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 1L;
+
+    private List<Movie> movieList;
+
+    @SpringBean(name = "videoFacade")
+    private VideoStoreFacade facade;
+
+    public MovieDataProvider() {
+	InjectorHolder.getInjector().inject(this);
+	movieList = facade.getMovies();
+	setSort(new SortParam("title",true));
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.wicket.markup.repeater.data.IDataProvider#iterator(int,
+     * int)
+     */
+    public Iterator<Movie> iterator(int first, int count) {
+	Collections.sort(movieList, new PropertyAwareComparator(getSort()));
+	return movieList.iterator();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.wicket.markup.repeater.data.IDataProvider#size()
+     */
+    public int size() {
+	return movieList.size();
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.apache.wicket.markup.repeater.data.IDataProvider#model(java.lang.
+     * Object)
+     */
+    public IModel<Movie> model(Movie movie) {
+	return Model.of(movie);
+    }
+
+    private class PropertyAwareComparator implements Comparator<Movie> {
+
+	private SortParam param;
+
+	public PropertyAwareComparator(SortParam param){
+	    this.param = param;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	public int compare(Movie o1, Movie o2) {
+	    boolean ascending = param.isAscending();
+	    String property = param.getProperty();
+
+	    PropertyModel<Comparable> m1 = new PropertyModel<Comparable>(o1, property);
+	    PropertyModel<Comparable> m2 = new PropertyModel<Comparable>(o2, property);
+
+	    int result = m1.getObject().compareTo(m2.getObject());
+	    if(!ascending)
+		result*=-1;
+	    return result;
+
+	}
+
+    }
+
+}
--- src/main/java/de/pentasys/k/pages/SelectMoviePage.java
+++ src/main/java/de/pentasys/k/pages/SelectMoviePage.java
@@ -3,15 +3,27 @@
  */
 package de.pentasys.k.pages;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.HeaderlessColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.HeadersToolbar;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
@@ -30,77 +42,180 @@ public class SelectMoviePage extends AbstractVideoStorePage {
     private VideoStoreFacade videoFacade;
 
     public SelectMoviePage() {
-	List<Movie> movieList = videoFacade.getMovies();
-	Form<List<? extends Movie>> form = new Form<List<? extends Movie>>(
-		"movieForm", Model.ofList(movieList));
-	ListView<Movie> listView;
-	form.add(listView = new PropertyListView<Movie>("movieList", Model
-		.ofList(movieList)) {
 
+	MovieDataProvider movieDataProvider =  new MovieDataProvider();
+
+	Form<List<? extends Movie>> form = new Form<List<? extends Movie>>(
+		"movieForm", Model.ofList(videoFacade.getMovies()));
+
+
+	DataTable<Movie> dataTable = new DataTable<Movie>("movieTable",createMovieColumnList(),movieDataProvider,5);
+	dataTable.setTableBodyCss("movieTable");
+	dataTable.addBottomToolbar(new HeadersToolbar(dataTable, movieDataProvider));
+	form.add(dataTable);
+	add(form);
+    }
+
+    /**
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    private IColumn<Movie>[] createMovieColumnList() {
+	List<IColumn<Movie>> columnList = new ArrayList<IColumn<Movie>>();
+	IColumn<Movie> titleColumn = new PropertyColumn<Movie>(new Model<String>("Titel"),"title","title"){
 	    /**
-	     * Serial Version UID
+	     *
 	     */
 	    private static final long serialVersionUID = 1L;
 
+	    /* (non-Javadoc)
+	     * @see org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn#getCssClass()
+	     */
 	    @Override
-	    protected void populateItem(final ListItem<Movie> item) {
+	    public String getCssClass() {
+	        return "movieColumnBig";
+	    }
+	};
+	IColumn<Movie> actorsColumn = new PropertyColumn<Movie>(new Model<String>("Schauspieler"),"actors"){
+	    /**
+	     *
+	     */
+	    private static final long serialVersionUID = 1L;
 
-		item.add(new Label("title"));
-		item.add(new PropertyListView<Actor>("actors", item
-			.getModelObject().getActors()) {
-		    /**
-		     * Serial Version UID
-		     */
-		    private static final long serialVersionUID = 1L;
-
-		    /*
-		     * (non-Javadoc)
-		     *
-		     * @see
-		     * org.apache.wicket.markup.html.list.ListView#populateItem
-		     * (org.apache.wicket.markup.html.list.ListItem)
-		     */
-		    @Override
-		    protected void populateItem(ListItem<Actor> item) {
-			item.add(new Label("name"));
-		    }
-		});
-		item.add(new Label("prizePerDay"));
-		AjaxLink<Void> link;
-		item.add(link = new AjaxLink<Void>("order", new Model()) {
-
-		    /**
-		     * Serial Version UID
-		     */
-		    private static final long serialVersionUID = 1L;
-
-		    @Override
-		    public void onClick(AjaxRequestTarget target) {
-			VideoSession.get().getSelectedMovies()
-				.add(item.getModelObject());
-			target.addComponent(this);
-		    }
-
-		    /*
-		     * (non-Javadoc)
-		     *
-		     * @see org.apache.wicket.Component#isVisible()
-		     */
-		    @Override
-		    public boolean isVisible() {
-			return !VideoSession.get().getSelectedMovies()
-				.contains(item.getModelObject());
-		    }
-
-		});
-
-		link.add(new Label("orderLink", "Bestellen"));
-
+	    /* (non-Javadoc)
+	     * @see org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn#populateItem(org.apache.wicket.markup.repeater.Item, java.lang.String, org.apache.wicket.model.IModel)
+	     */
+	    @Override
+	    public void populateItem(Item<ICellPopulator<Movie>> item,
+	            String componentId, IModel<Movie> rowModel) {
+		item.add(new ActorsFragment(componentId,SelectMoviePage.this, new Model((Serializable)rowModel.getObject().getActors())));
 	    }
 
-	});
-	listView.setReuseItems(true);
-	add(form);
+	    /* (non-Javadoc)
+	     * @see org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn#getCssClass()
+	     */
+	    @Override
+	    public String getCssClass() {
+	        return "movieColumnMedium";
+	    }
+	};
+	IColumn<Movie> prizeColumn = new PropertyColumn<Movie>(new Model<String>("Preis / Tag"), "prizePerDay", "prizePerDay"){
+	    /**
+	     *
+	     */
+	    private static final long serialVersionUID = 1L;
+
+	    /* (non-Javadoc)
+	     * @see org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn#getCssClass()
+	     */
+	    @Override
+	    public String getCssClass() {
+	        return "movieColumnSmall";
+	    }
+	};
+	IColumn<Movie> submitColumn = new HeaderlessColumn<Movie>() {
+
+	    /**
+	     *
+	     */
+	    private static final long serialVersionUID = 1L;
+
+	    public void populateItem(Item<ICellPopulator<Movie>> cellItem,
+		    String componentId, IModel<Movie> rowModel) {
+		cellItem.add(new LinkFragment(componentId,SelectMoviePage.this, rowModel));
+	    }
+
+	    /* (non-Javadoc)
+	     * @see org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractColumn#getCssClass()
+	     */
+	    @Override
+	    public String getCssClass() {
+	        return "movieColumnSmall";
+	    }
+
+	};
+
+	columnList.add(titleColumn);
+	columnList.add(actorsColumn);
+	columnList.add(prizeColumn);
+	columnList.add(submitColumn);
+
+	return columnList.toArray(new IColumn[0]);
+    }
+
+    private static class ActorsFragment extends Fragment {
+
+	/**
+	 * @param id
+	 * @param markupId
+	 * @param markupProvider
+	 */
+	public ActorsFragment(String id,
+		MarkupContainer markupProvider, IModel<List<Actor>> model) {
+	    super(id, "actorsListFragment", markupProvider, model);
+	    add(new PropertyListView<Actor>("actorsList", model){
+
+		/**
+		 * Serial Version UID
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		protected void populateItem(ListItem<Actor> item) {
+		    item.add(new Label("name"));
+		}
+
+	    });
+	}
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+    }
+
+    private static class LinkFragment extends Fragment {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @param id
+	 * @param markupId
+	 * @param markupProvider
+	 */
+	public LinkFragment(String id, MarkupContainer markupProvider, IModel<Movie> model) {
+	    super(id, "linkFragment", markupProvider , model);
+	    AjaxLink<Movie> link;
+	    add(link = new AjaxLink<Movie>("order", model){
+
+		/**
+		 * Serial Version UID
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void onClick(AjaxRequestTarget target) {
+		    VideoSession.get().getSelectedMovies().add(getModelObject());
+		    target.addComponent(this);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.apache.wicket.Component#isVisible()
+		 */
+		@Override
+		public boolean isVisible() {
+		    Set<Movie> selectedMovies = VideoSession.get().getSelectedMovies();
+		    return !selectedMovies.contains(getModelObject());
+		}
+	    });
+	    link.add(new Label("title", "Bestellen"));
+	}
+
     }
 
 }
